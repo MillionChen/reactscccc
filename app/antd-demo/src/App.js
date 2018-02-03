57809e28422b6a976acf03c7966aa12dd08e91f2
MillionChen <450413474@qq.com>
2018-02-03 09:01:27 +0800

    app

---
+++ app/antd-demo/src/App.js
@@ -0,0 +1,16 @@
+import React ,{ Component} from "react";
+import ReactDom,{render}  from "react-dom";
+import { Carousel } from 'antd';
+
+function onChange(a, b, c) {
+    console.log(a, b, c);
+}
+
+ReactDom.render(
+    <Carousel afterChange={onChange}>
+        <div><h3>1</h3></div>
+        <div><h3>2</h3></div>
+        <div><h3>3</h3></div>
+        <div><h3>4</h3></div>
+    </Carousel>
+    , window.root);
---
+++ app/antd-demo/src/index.js
@@ -0,0 +1,17 @@
+import React ,{ Component} from "react";
+import ReactDom,{render}  from "react-dom";
+import { Carousel } from 'antd';
+import "./App.css";
+
+function onChange(a, b, c) {
+    console.log(a, b, c);
+}
+
+ReactDom.render(
+    <Carousel afterChange={onChange}>
+        <div><h3>1</h3></div>
+        <div><h3>2</h3></div>
+        <div><h3>3</h3></div>
+        <div><h3>4</h3></div>
+    </Carousel>
+    , window.root);
